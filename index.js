f68cfb695b974606dce4aca17dd9c4882590f662
kennyOlakunle <olakunlemuyiwa@gmail.com>
2021-01-11 12:33:26 +0100

    updating index

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const initialRoutes = require('./routes/web');
+const initialRoutes = require('./src/routes/web');
 
 app.use(express.urlencoded({ extended: true }));
 initialRoutes(app);
 
-let port = PROCESS.ENV.PORT || 3000;
+let port = 3000;
 app.listen(port, () => {
   console.log(`Running at localhost:${port}`);
 });
